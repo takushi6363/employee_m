ae4682fd7ab3a4e6d85e36e2c9cd7030c637b0e3
takushi_akayama <akayama63@gmail.com>
2021-05-23 13:31:27 +0900

    マイナンバーカラム削除に伴い変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :working_days
   has_many :books
+  has_many :vacations
 
   validates :name, presence: true
   validates :working_days_id, numericality: { other_than: 1 } 
 
-  attr_encrypted :my_number, key: 'This is a key that is 256 bits!!'
 
 end
