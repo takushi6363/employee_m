ea5a57dc35605cd0267de0b65fe9e231d8f321b9
takushi_akayama <akayama63@gmail.com>
2021-05-23 13:35:43 +0900

    その他仕様変更にて変更箇所

---
+++ test/controllers/paid_explanations_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PaidExplanationsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/controllers/paid_vacations_all_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PaidVacationsAllControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
