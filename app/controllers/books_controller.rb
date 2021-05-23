7e992f2bfa28b5986cbc15684cbca284adee339b
takushi_akayama <akayama63@gmail.com>
2021-05-23 13:23:21 +0900

    books モデルにコードを移行

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,8 @@
 class BooksController < ApplicationController
-
+  before_action :user_set_up, only:[:index,:create,:search]
+  before_action :set_up,only:[:index,:create,:search]
+  
 def index
-  @user = User.find(params[:user_id])
   @books_all = Book.where(user_id: params[:user_id]).order(purchase_date: :desc)
   @time_year = Time.new.year
 end
@@ -20,14 +21,13 @@ def destroy
 end
 
 def search
-  @user = User.find(params[:user_id])
   @time_year = Time.new.year
    if params[:year] == ""
     @books_all = Book.where(user_id: params[:user_id]).order(purchase_date: :desc)
    else
     @year = params[:year].to_i
-    time_a =Time.zone.parse("#{@year - 1 }-07-01")
-    time_b =Time.zone.parse("#{@year}-06-30")
+    time_a =Time.zone.parse("#{@year}-07-01")
+    time_b =Time.zone.parse("#{@year + 1}-06-30")
     @books_all = Book.where(user_id: params[:user_id]).where(purchase_date: time_a..time_b).order(purchase_date: :desc)
    end
    render template: "books/index"
@@ -35,32 +35,16 @@ end
 
     private
 
+    def set_up
+      @books = Book.books_tims(@user.id)
+    end
+
+    def user_set_up
+      @user = User.find(params[:user_id])
+    end
+
     def book_params
       params.permit(:price,:purchase_date).merge(user_id: @user.id)
     end
 
 end
-
-def book_expenses_same_one
-  require "time"
-  time_now = Time.new
-  month  = time_now.month
-  if month == 7 ||  month == 8 || month == 9 || month == 10 ||month == 11 || month == 12
-  time_a =Time.zone.parse("#{time_now.year}-07-01")
-  @books = Book.where(user_id: params[:user_id]).where(purchase_date: time_a..time_now).order(purchase_date: :desc)
-  else
-  time_b = Time.zone.parse("#{time_now.year - 1}-07-01")
-  @books = Book.where(user_id: params[:user_id]).where(purchase_date: time_b..time_now).order(purchase_date: :desc)
-  end
-  num = 0
-  @books.each do |book|
-    num += book.price
-  end
-  num
-end
-
-def book_balance
-  30000 - book_expenses_same_one
-end
-
-
