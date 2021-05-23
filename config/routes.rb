53c717653d0e77c02ca1581f739d918af9f72d82
takushi_akayama <akayama63@gmail.com>
2021-05-23 13:31:06 +0900

    システム追記に伴い変更

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,13 @@ Rails.application.routes.draw do
   delete  'logout',  to: 'sessions#destroy'
   resources :master, only: :index
   resources :books_all, only: [:index]
+  resources :paid_vacations_all, only:[:index]
+    resources :paid_explanations, only:[:index]
   resources :users do
   resources :books, only: [:index,:create,:destroy,:show] do
+    get :search, on: :collection
+  end
+  resources :paid_vacations, only:[:index,:create,:destroy,] do
     get :search, on: :collection
   end
   resources :homes, only: [:index,:create,:destroy]
