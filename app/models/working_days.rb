c2e9d0fce937d5adee0de1eeaaa83c88e284e84e
takushi_akayama <akayama63@gmail.com>
2021-05-31 21:59:39 +0900

    選択項目名修正

--- app/models/working_days.rb
+++ app/models/working_days.rb
@@ -1,11 +1,11 @@
 class WorkingDays < ActiveHash::Base
   self.data = [
     { id: 1, name: '有給休暇の日数の選択' },
-    { id: 2, name: '正職員（5日勤務）' },
-    { id: 3, name: '有給日数 4/5' },
-    { id: 4, name: '有給日数 3/5' },
-    { id: 5, name: '有給日数 2/5' },
-    { id: 6, name: '有給日数 1/5' },
+    { id: 2, name: '正職員' },
+    { id: 3, name: '週４日勤務（有給休暇 4/5)' },
+    { id: 4, name: '週３日勤務（有給日数 3/5）' },
+    { id: 5, name: '週２日勤務（有給日数 2/5）' },
+    { id: 6, name: '週１日勤務（有給日数 1/5）' },
   ]
 
   include ActiveHash::Associations
