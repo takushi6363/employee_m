9b38360f329961b72d1b732d5033cfa3958981ba
takushi_akayama <akayama63@gmail.com>
2021-06-02 17:26:53 +0900

    電話番号のバリデーション追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :joining_day, presence: true
   validates :working_days_id, numericality: { other_than: 1, message: 'を選択して下さい' }
+  validates :phone_number, format: { with: /\A[0-9-]{,14}\z/,message: 'は半角数字とハイフンで入力して下さい'}
+  validates :phone_number_2, format: { with: /\A[0-9-]{,14}\z/,message: 'は半角数字とハイフンで入力して下さい'}
 end
