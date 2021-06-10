7c8f52f616991c4da53a46db20de98ad700822f3
takushi_akayama <akayama63@gmail.com>
2021-06-10 18:01:07 +0900

    記載の修正

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,6 @@
 class HomesController < ApplicationController
   def index
     @books = Book.where(user_id: params[:user_id])
-    @user = User.find(params[:user_id])
     @time_year = Time.new.year
   end
 end
